170de2c6fcec36d2ac36795a5a1fc01077a4d1ea
VatashchukD <154114170+VatashchukD@users.noreply.github.com>
2024-05-29 12:19:08 +0500

    Add files via upload

---
+++ bottom-sheet.js
@@ -0,0 +1,10 @@
+document.getElementById("navigation__menu-button").addEventListener("click",function(){
+    this.classList.toggle("open");
+    document.querySelector(".navigation__wrap").classList.toggle("open");
+    document.querySelector(".navigation__menu").classList.toggle("open");
+    document.querySelector(".menu").classList.toggle("open");
+    document.querySelector(".navigation__refs-list").classList.toggle("open");
+    document.body.classList.toggle("hidden");
+    document.querySelector("html").classList.toggle("hidden");
+    
+})
